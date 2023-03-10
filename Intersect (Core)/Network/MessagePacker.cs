331e778fa93925aea80dcb7d4249b3b49043bb30
Robbie Lodico <pandacoder@pm.me>
2023-03-09 23:52:35 -0500

    fix: make NetAES work between net7.0 and net462 - log lines (commented out) for debugging lidgren - disable encryption, client can communicate with server - update to new version of lidgren with fix

--- Intersect (Core)/Network/MessagePacker.cs
+++ Intersect (Core)/Network/MessagePacker.cs
@@ -14,35 +14,53 @@ namespace Intersect.Network
     {
         public static readonly MessagePacker Instance = new MessagePacker();
 
-        private readonly MessagePackSerializerOptions mOptions  = MessagePackSerializerOptions.Standard.
+        private static readonly MessagePackSerializerOptions _options = MessagePackSerializerOptions.Standard.
             WithResolver(MessagePack.Resolvers.CompositeResolver.Create(
-                new IFormatterResolver[] { 
-                    MessagePack.Resolvers.NativeGuidResolver.Instance, 
-                    MessagePack.Resolvers.NativeDateTimeResolver.Instance, 
-                    MessagePack.Resolvers.NativeDecimalResolver.Instance, 
+                new IFormatterResolver[] {
+                    MessagePack.Resolvers.NativeGuidResolver.Instance,
+                    MessagePack.Resolvers.NativeDateTimeResolver.Instance,
+                    MessagePack.Resolvers.NativeDecimalResolver.Instance,
                     MessagePack.Resolvers.StandardResolver.Instance }
                 )
             );
 
-        private readonly MessagePackSerializerOptions mCompressedOptions = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4Block);
+        private readonly MessagePackSerializerOptions _compressionOptions = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4Block);
 
-        public byte[] Serialize(IntersectPacket pkt)
+        public byte[] Serialize(IntersectPacket packet)
         {
-            var packedPacket = new PackedIntersectPacket(pkt)
-            {
-                Data = MessagePackSerializer.Serialize((object)pkt, mOptions)
-            };
+            // Log.Error($"{packet.GetType().FullName} Length: {packet.Data.Length} Data: {BitConverter.ToString(packet.Data)}");
+            // Log.Error($"{packet.GetType().FullName}");
 
-            return MessagePackSerializer.Serialize((object)packedPacket, mCompressedOptions);
+            var packedPacket = new PackedIntersectPacket(packet)
+            {
+                Data = MessagePackSerializer.Serialize(packet.GetType(), packet, _options)
+            };
+            // Log.Error($"{packedPacket.GetType().FullName} Length: {packedPacket.Data.Length} Data: {BitConverter.ToString(packedPacket.Data)}");
+
+            var data = MessagePackSerializer.Serialize(packedPacket, _compressionOptions);
+            // Log.Error($"<byte[]> Length: {data.Length} Data: {BitConverter.ToString(data)}");
+
+            return data;
         }
 
         public object Deserialize(byte[] data)
         {
             try
             {
-                var packedPacket = MessagePackSerializer.Deserialize<PackedIntersectPacket>(data, mCompressedOptions);
-                var intersectPacket = MessagePackSerializer.Deserialize(packedPacket.PacketType, packedPacket.Data, mOptions);
-                return intersectPacket;
+                // Log.Error($"<byte[]> Length: {data.Length} Data: {BitConverter.ToString(data)}");
+
+                var packedPacket = MessagePackSerializer.Deserialize<PackedIntersectPacket>(data, _compressionOptions);
+                // Log.Error($"{packedPacket.GetType().FullName} Length: {packedPacket.Data.Length} Data: {BitConverter.ToString(packedPacket.Data)}");
+
+                var deserializedPacket = MessagePackSerializer.Deserialize(packedPacket.PacketType, packedPacket.Data, _options);
+                if (deserializedPacket is IntersectPacket packet)
+                {
+                    // Log.Error($"{packet.GetType().FullName} Length: {packet.Data.Length} Data: {BitConverter.ToString(packet.Data)}");
+                    return packet;
+                }
+                
+                // Log.Error($"Deserialized packet is actually {deserializedPacket.GetType().FullName}");
+                return null;
             }
             catch (Exception exception)
             {
