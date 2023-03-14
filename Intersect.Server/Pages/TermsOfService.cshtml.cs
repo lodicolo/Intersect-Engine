d93b15702267877bcffbc353590aa3d2fcd2ad95
Robbie Lodico <pandacoder@pm.me>
2023-03-14 00:16:30 -0400

    feat: layout, login and registration

---
+++ Intersect.Server/Pages/TermsOfService.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Intersect.Server.Pages;
+
+public class TermsOfServiceModel : PageModel
+{
+    private readonly ILogger<TermsOfServiceModel> _logger;
+
+    public TermsOfServiceModel(ILogger<TermsOfServiceModel> logger)
+    {
+        _logger = logger;
+    }
+}
