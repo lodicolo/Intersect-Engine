4c9073d0721da476ac9337fbbfe46a9ca1ae4389
Robbie Lodico <pandacoder@pm.me>
2023-03-21 21:51:23 -0400

    feat: Intersect Release Bundler
    prototype bundler
    
    fix: change input args
    
    fix: moved version to be a parameter
    
    make the bundle name part of the extension
    
    change debug to info, correct some pathing
    
    more logging, possible fixes
    
    more logging, more hopeful fixes
    
    make fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes
    
    fixes

---
+++ src/bundle.ts
@@ -0,0 +1,204 @@
+import {info, warning} from '@actions/core';
+import {copyFile, open, stat} from 'fs/promises';
+import {glob} from 'glob';
+import {mkdirp} from 'mkdirp';
+import {basename, dirname, join, normalize, relative, sep} from 'path';
+import archiver from 'archiver';
+
+export type Result<T = void> =
+	| (T extends void ? {ok: true} : {ok: true; value: T})
+	| {ok: false; err: Error};
+
+export type FileMapping = {
+	source: string;
+	target: string;
+};
+
+export type DirectorySegments = string | string[];
+
+export type BundleDescriptor = {
+	directories: DirectorySegments[];
+	includes: (FileMapping | string)[];
+	name: string;
+};
+
+export async function makeDirectory(
+	parent: string,
+	segments: DirectorySegments
+): Promise<Result> {
+	try {
+		if (typeof segments === 'string') {
+			const resolvedPath = join(parent, segments);
+			info(`Creating directory: ${resolvedPath}`);
+			await mkdirp(resolvedPath);
+			return {ok: true};
+		}
+
+		if (!Array.isArray(segments)) {
+			throw new Error(`Invalid segment: ${segments}`);
+		}
+
+		const resolvedPath = join(parent, ...segments);
+		info(`Creating directory: ${resolvedPath}`);
+		await mkdirp(resolvedPath);
+		return {ok: true};
+	} catch (err) {
+		return {
+			ok: false,
+			err: err instanceof Error ? err : new Error(err as string)
+		};
+	}
+}
+
+export async function packageBundle(
+	version: string,
+	...bundleDescriptors: BundleDescriptor[]
+): Promise<Result<string[]>> {
+	const repositoryRoot = process.cwd();
+	info(`Bundling packages from ${repositoryRoot}`);
+
+	const archivePaths: string[] = [];
+	for (const {directories, includes, name} of bundleDescriptors) {
+		const bundleOutputDirectory = join('dist', name);
+		await mkdirp(bundleOutputDirectory);
+
+		for (const nestableDirectory of directories) {
+			const result = await makeDirectory(
+				bundleOutputDirectory,
+				nestableDirectory
+			);
+			if (result.ok !== true) {
+				return {
+					ok: false,
+					err: result.err
+				};
+			}
+		}
+
+		for (const include of includes) {
+			if (typeof include === 'object') {
+				const normalizedIncludeSource = normalize(include.source);
+				info(
+					`Searching for "${normalizedIncludeSource}" (${include.source})...`
+				);
+				const globbedPaths = await glob(include.source, {
+					absolute: true,
+					cwd: repositoryRoot,
+					nodir: true,
+					realpath: true
+				});
+				const resolvedTarget = join(
+					repositoryRoot,
+					bundleOutputDirectory,
+					include.target
+				);
+				if (globbedPaths.length > 0) {
+					if (globbedPaths.length > 1) {
+						await mkdirp(include.target);
+					}
+
+					info(
+						`Found paths for glob '${include.source}': ${JSON.stringify(
+							globbedPaths,
+							null,
+							2
+						)}`
+					);
+
+					for (const globbedFilePath of globbedPaths) {
+						const globbedFileName = basename(globbedFilePath);
+						info(`Resolved ${globbedFileName} (${globbedFilePath})`);
+						let targetFilePath = join(resolvedTarget, globbedFileName);
+						if (normalizedIncludeSource.endsWith('**')) {
+							const absoluteSourceDirectory = dirname(
+								join(repositoryRoot, normalizedIncludeSource)
+							);
+							const relativeGlobbedFilePath = relative(
+								absoluteSourceDirectory,
+								globbedFilePath
+							);
+							info(`Relative globbed file path: ${relativeGlobbedFilePath}`);
+							const resolvedTargetGlobbedFilePath = join(
+								resolvedTarget,
+								relativeGlobbedFilePath
+							);
+							info(
+								`Resolved target globbed file path: ${resolvedTargetGlobbedFilePath}`
+							);
+							if (relativeGlobbedFilePath.includes(sep)) {
+								const resolvedTargetGlobbedFileDirName = dirname(
+									resolvedTargetGlobbedFilePath
+								);
+								info(`mkdirp: ${resolvedTargetGlobbedFileDirName}`);
+								await mkdirp(resolvedTargetGlobbedFileDirName);
+							}
+							info(
+								`Re-resolved ${relativeGlobbedFilePath} to ${resolvedTargetGlobbedFilePath}`
+							);
+							targetFilePath = resolvedTargetGlobbedFilePath;
+						} else if (globbedPaths.length === 1) {
+							try {
+								const stats = await stat(resolvedTarget);
+								if (stats === null || !stats.isDirectory()) {
+									targetFilePath = resolvedTarget;
+								}
+							} catch (err) {
+								if (
+									!err ||
+									typeof err !== 'object' ||
+									!('code' in err) ||
+									err.code !== 'ENOENT'
+								) {
+									warning(err as string | Error);
+								}
+								targetFilePath = resolvedTarget;
+							}
+						}
+						info(`Copying ${include.source} to ${targetFilePath}`);
+						await copyFile(globbedFilePath, targetFilePath);
+					}
+				} else {
+					info(`No files found for: ${include.source}`);
+				}
+			}
+		}
+
+		try {
+			const archiveName = `intersect-${version}.${name}.zip`;
+			const archivePath = join(repositoryRoot, 'dist', archiveName);
+			const fileHandle = await open(archivePath, 'w');
+			const writeStream = fileHandle.createWriteStream();
+			const archive = archiver('zip');
+
+			writeStream.on('close', () => {
+				info(`Wrote ${archive.pointer()}B to ${archiveName}`);
+			});
+
+			archive.on('warning', err => {
+				if (err.code === 'ENOENT') {
+					warning(err);
+				} else {
+					throw err;
+				}
+			});
+
+			archive.pipe(writeStream);
+			const directoryToArchive = join(repositoryRoot, 'dist', name);
+			info(`Adding directory to archive: ${directoryToArchive}`);
+			archive.directory(directoryToArchive, false);
+			await archive.finalize();
+
+			archivePaths.push(bundleOutputDirectory);
+		} catch (err) {
+			return {
+				ok: false,
+				err: err instanceof Error ? err : new Error(err as string)
+			};
+		}
+	}
+
+	return {
+		ok: true,
+		value: archivePaths
+	};
+}
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,62 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
+import {error, getInput, info, setFailed, setOutput} from '@actions/core';
+import {glob} from 'glob';
+import {readFile} from 'fs/promises';
+import {BundleDescriptor, Result, packageBundle} from './bundle';
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
+async function readBundle(
+	bundleFilePath: string
+): Promise<Result<BundleDescriptor>> {
+	try {
+		const bundleFileRaw = await readFile(bundleFilePath, 'utf8');
+		const bundleDescriptor: BundleDescriptor = JSON.parse(bundleFileRaw);
+		return {
+			ok: true,
+			value: bundleDescriptor
+		};
+	} catch (err) {
+		return {
+			ok: false,
+			err: err instanceof Error ? err : new Error(err as string)
+		};
+	}
 }
 
-run()
+async function run(): Promise<void> {
+	try {
+		const bundleFilePath: string = getInput('bundle');
+		const globbedBundleFilePaths = await glob(bundleFilePath);
+		const bundleDescriptors: BundleDescriptor[] = [];
+
+		let failed = false;
+		for (const globbedBundleFilePath of globbedBundleFilePaths) {
+			const result = await readBundle(globbedBundleFilePath);
+			if (result.ok) {
+				bundleDescriptors.push(result.value);
+			} else {
+				error(result.err);
+				failed = true;
+			}
+		}
+
+		if (failed) {
+			setFailed('One or more bundle files could not be read.');
+			return;
+		}
+
+		const version: string = getInput('version');
+
+		const result = await packageBundle(version, ...bundleDescriptors);
+		if (!result.ok) {
+			error(result.err);
+			setFailed('One or more bundles could not be packaged.');
+			return;
+		}
+
+		info('Finished packaging bundles.');
+		setOutput('archives', result.value);
+	} catch (err) {
+		setFailed(err as string | Error);
+	}
+}
+
+run();
